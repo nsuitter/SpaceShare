a749e5046f9da056e2817d7c20b37e1e808af54d
nsuitter <nsuitter@saleforce.com>
2015-02-11 10:28:15 -0800

    Integrating bootstrap and creating about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :zips
   resources :listings
+  get 'about', to: 'static_pages#about'
   root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
